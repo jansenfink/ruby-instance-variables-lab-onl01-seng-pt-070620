0779be7579b94992c8ffc388cbafc3eb7467d4d3
Jansen Fink <jansenfink@charter.net>
2020-07-22 19:52:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 # A local variable that is defined inside one method can't be accessed by another method - workaround = instance local_variables
 
-class Dog 
-  def name = (dog_name)
-    this_dogs_name = dog_name
-  end 
-  
-  def name 
-    this_dogs_name
-  end 
-end 
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
